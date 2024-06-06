343abb0e64af4bbb5b8e1367e81499f182b5c8a2
Bob <>
2024-06-06 21:59:59 +0300

    validate headers

--- cypress/e2e/api.cy.js
+++ cypress/e2e/api.cy.js
@@ -1,5 +1,24 @@
-describe('template spec', () => {
+/// <reference types = "cypress" />
+
+describe('Learn REST API Testing with Cypress', () => {
   it('passes', () => {
-    cy.visit('https://example.cypress.io')
+    cy.request('/users/2').then((response) => {
+      cy.log(JSON.stringify(response.body.data.email))
+      cy.log(JSON.stringify(response.headers))
+    })
+  })
+
+  it('API Tests - Validate Headers', () => {
+    cy.request('/user/2').as('whateverUser')
+    cy.get('@whateverUser')
+      .its('headers')
+      .its('content-type')
+      .should('include', 'application/json')
+
+    cy.get('@whateverUser')
+      .its('headers')
+      .its('connection')
+      .should('include', 'keep-alive')
+
   })
 })
